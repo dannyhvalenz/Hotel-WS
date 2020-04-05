9d22b8de9781a1fc5f60d0f8837b80cafc8df60a
Daniela H Valenzuela <dannyhvalenz@live.com.mx>
2020-04-05 03:45:57 -0500

    Update hotel.sql
    Registros de clientes y reservaciones

--- database/hotel.sql
+++ database/hotel.sql
@@ -4,14 +4,14 @@
  Source Server         : AWS
  Source Server Type    : MySQL
  Source Server Version : 50729
- Source Host           : localhost:3306
+ Source Host           : 54.162.225.248:3306
  Source Schema         : hotel
 
  Target Server Type    : MySQL
  Target Server Version : 50729
  File Encoding         : 65001
 
- Date: 04/04/2020 21:26:15
+ Date: 05/04/2020 03:45:31
 */
 
 SET NAMES utf8mb4;
@@ -22,14 +22,14 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `clientes`;
 CREATE TABLE `clientes` (
-  `idCliente` int(11) NOT NULL,
+  `idCliente` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(45) NOT NULL,
   `apellido` varchar(100) NOT NULL,
   `correo` varchar(100) NOT NULL,
   `telefono` varchar(45) NOT NULL,
   `formaPago` varchar(45) NOT NULL,
   PRIMARY KEY (`idCliente`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=111 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of clientes
@@ -124,17 +124,7 @@ INSERT INTO `clientes` VALUES (86, 'Quinlan', 'Salinas', 'a.tortor.Nunc@gravidam
 INSERT INTO `clientes` VALUES (87, 'Stone', 'Reyes', 'sollicitudin@lectusquismassa.co.uk', '574-820-5687', 'Efectivo');
 INSERT INTO `clientes` VALUES (88, 'Sasha', 'Mckay', 'in.faucibus.orci@faucibus.edu', '980-479-9306', 'Credito');
 INSERT INTO `clientes` VALUES (89, 'Leah', 'Walker', 'leo@Nunc.net', '143-912-3492', 'Debito');
-INSERT INTO `clientes` VALUES (90, 'Rhea', 'Tillman', 'orci@vulputate.com', '937-497-2025', 'Efectivo');
-INSERT INTO `clientes` VALUES (91, 'Jamal', 'Hardy', 'vel.arcu@nullaIntegerurna.com', '328-184-5719', 'Debito');
-INSERT INTO `clientes` VALUES (92, 'Bruce', 'Chase', 'a@cursuseteros.co.uk', '493-704-8281', 'Debito');
-INSERT INTO `clientes` VALUES (93, 'Aline', 'Yates', 'Mauris@Aeneanmassa.org', '833-859-6806', 'Efectivo');
-INSERT INTO `clientes` VALUES (94, 'Mara', 'Peck', 'egestas@euaugueporttitor.co.uk', '678-700-8276', 'Efectivo');
-INSERT INTO `clientes` VALUES (95, 'Clementine', 'Chase', 'odio.Aliquam@eget.edu', '837-897-0935', 'Debito');
-INSERT INTO `clientes` VALUES (96, 'Kamal', 'Rosales', 'adipiscing.Mauris.molestie@commodotincidunt.com', '971-945-3082', 'Debito');
 INSERT INTO `clientes` VALUES (97, 'Dalton', 'Mcdowell', 'sed.sapien@nisl.ca', '641-353-5135', 'Credito');
-INSERT INTO `clientes` VALUES (98, 'Eliana', 'Riddle', 'tempor.lorem@et.com', '714-131-5970', 'Debito');
-INSERT INTO `clientes` VALUES (99, 'Cheryl', 'Mcgee', 'accumsan.neque@acmattisvelit.edu', '119-251-1102', 'Efectivo');
-INSERT INTO `clientes` VALUES (100, 'Hedwig', 'Vance', 'faucibus@a.org', '458-452-5607', 'Efectivo');
 COMMIT;
 
 -- ----------------------------
@@ -152,15 +142,63 @@ CREATE TABLE `estancias` (
   `idCliente` int(11) NOT NULL,
   PRIMARY KEY (`idEstancia`),
   KEY `idClienteEstancia` (`idCliente`),
-  KEY `numHabitacion` (`numHabitacion`),
-  CONSTRAINT `idClienteEstancia` FOREIGN KEY (`idCliente`) REFERENCES `clientes` (`idCliente`),
-  CONSTRAINT `numHabitacion` FOREIGN KEY (`numHabitacion`) REFERENCES `habitaciones` (`numHabitacion`)
+  KEY `numHabitacion` (`numHabitacion`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of estancias
 -- ----------------------------
 BEGIN;
+INSERT INTO `estancias` VALUES (1, 44, 4, 1, 2013, '2021-02-20', '2020-12-09', 27);
+INSERT INTO `estancias` VALUES (2, 22, 6, 0, 5947, '2020-05-12', '2019-07-19', 78);
+INSERT INTO `estancias` VALUES (3, 52, 8, 0, 2693, '2019-09-15', '2020-11-22', 3);
+INSERT INTO `estancias` VALUES (4, 34, 4, 1, 7875, '2019-11-14', '2020-10-25', 20);
+INSERT INTO `estancias` VALUES (5, 79, 7, 0, 7097, '2019-12-05', '2019-09-29', 86);
+INSERT INTO `estancias` VALUES (6, 84, 4, 0, 6582, '2021-03-28', '2019-09-14', 53);
+INSERT INTO `estancias` VALUES (7, 76, 8, 2, 2186, '2019-06-08', '2019-07-05', 62);
+INSERT INTO `estancias` VALUES (8, 82, 3, 0, 2642, '2020-03-27', '2020-12-28', 35);
+INSERT INTO `estancias` VALUES (9, 93, 3, 1, 6268, '2019-08-12', '2020-07-13', 15);
+INSERT INTO `estancias` VALUES (10, 93, 4, 0, 4695, '2020-02-24', '2020-07-10', 32);
+INSERT INTO `estancias` VALUES (11, 6, 6, 2, 9079, '2020-09-30', '2020-12-19', 6);
+INSERT INTO `estancias` VALUES (12, 57, 4, 1, 9229, '2020-12-04', '2019-10-01', 7);
+INSERT INTO `estancias` VALUES (13, 10, 1, 2, 2264, '2020-08-05', '2021-01-15', 44);
+INSERT INTO `estancias` VALUES (14, 96, 1, 2, 4389, '2019-12-03', '2020-12-18', 79);
+INSERT INTO `estancias` VALUES (15, 18, 8, 0, 7038, '2020-02-03', '2020-08-11', 6);
+INSERT INTO `estancias` VALUES (16, 88, 2, 0, 9452, '2020-07-31', '2020-12-19', 16);
+INSERT INTO `estancias` VALUES (17, 7, 6, 1, 6841, '2019-06-15', '2021-03-25', 61);
+INSERT INTO `estancias` VALUES (18, 32, 4, 2, 6208, '2020-05-13', '2020-01-20', 61);
+INSERT INTO `estancias` VALUES (19, 55, 5, 1, 9825, '2019-08-02', '2021-03-25', 83);
+INSERT INTO `estancias` VALUES (20, 90, 3, 2, 5885, '2021-01-19', '2020-09-09', 98);
+INSERT INTO `estancias` VALUES (21, 97, 7, 1, 4345, '2019-05-26', '2019-12-14', 21);
+INSERT INTO `estancias` VALUES (22, 84, 6, 1, 8032, '2019-10-02', '2019-06-22', 78);
+INSERT INTO `estancias` VALUES (23, 73, 2, 1, 9204, '2020-04-08', '2020-11-02', 8);
+INSERT INTO `estancias` VALUES (24, 11, 8, 2, 9346, '2019-06-18', '2021-03-12', 18);
+INSERT INTO `estancias` VALUES (25, 61, 1, 2, 6418, '2020-05-11', '2019-09-13', 95);
+INSERT INTO `estancias` VALUES (26, 59, 2, 2, 3010, '2019-11-04', '2019-09-29', 9);
+INSERT INTO `estancias` VALUES (27, 61, 5, 1, 4362, '2021-01-22', '2019-09-18', 71);
+INSERT INTO `estancias` VALUES (28, 65, 8, 0, 8129, '2020-03-23', '2021-04-09', 31);
+INSERT INTO `estancias` VALUES (29, 51, 8, 2, 9812, '2020-07-28', '2020-01-07', 53);
+INSERT INTO `estancias` VALUES (30, 31, 4, 0, 8786, '2019-12-28', '2020-07-21', 13);
+INSERT INTO `estancias` VALUES (31, 96, 4, 1, 9680, '2020-10-23', '2020-11-23', 46);
+INSERT INTO `estancias` VALUES (32, 2, 8, 0, 6622, '2020-08-15', '2019-05-04', 44);
+INSERT INTO `estancias` VALUES (33, 31, 1, 0, 8430, '2019-05-30', '2020-10-21', 5);
+INSERT INTO `estancias` VALUES (34, 45, 6, 1, 7520, '2020-10-08', '2020-07-03', 34);
+INSERT INTO `estancias` VALUES (35, 70, 8, 0, 4640, '2020-01-22', '2019-04-16', 35);
+INSERT INTO `estancias` VALUES (36, 16, 7, 0, 9649, '2020-08-21', '2019-07-28', 87);
+INSERT INTO `estancias` VALUES (37, 6, 4, 1, 2102, '2021-01-14', '2019-04-25', 52);
+INSERT INTO `estancias` VALUES (38, 54, 1, 1, 5786, '2020-11-25', '2020-05-18', 22);
+INSERT INTO `estancias` VALUES (39, 45, 6, 1, 7561, '2020-07-21', '2020-01-15', 29);
+INSERT INTO `estancias` VALUES (40, 56, 2, 0, 2225, '2020-01-04', '2019-09-11', 27);
+INSERT INTO `estancias` VALUES (41, 61, 1, 1, 5683, '2019-04-16', '2020-06-04', 2);
+INSERT INTO `estancias` VALUES (42, 7, 4, 2, 5167, '2019-04-16', '2019-09-17', 45);
+INSERT INTO `estancias` VALUES (43, 21, 3, 0, 7564, '2021-03-20', '2021-03-30', 45);
+INSERT INTO `estancias` VALUES (44, 19, 4, 1, 6382, '2020-12-18', '2019-09-22', 12);
+INSERT INTO `estancias` VALUES (45, 99, 3, 2, 5057, '2020-04-01', '2019-04-18', 94);
+INSERT INTO `estancias` VALUES (46, 75, 4, 0, 4931, '2020-08-16', '2021-02-21', 41);
+INSERT INTO `estancias` VALUES (47, 88, 1, 2, 3209, '2020-12-08', '2021-03-13', 76);
+INSERT INTO `estancias` VALUES (48, 76, 4, 2, 8369, '2020-01-29', '2019-08-22', 53);
+INSERT INTO `estancias` VALUES (49, 85, 5, 0, 6505, '2020-06-15', '2019-04-26', 28);
+INSERT INTO `estancias` VALUES (50, 24, 1, 2, 7474, '2020-03-25', '2019-05-24', 46);
 COMMIT;
 
 -- ----------------------------
@@ -188,7 +226,7 @@ COMMIT;
 -- ----------------------------
 DROP TABLE IF EXISTS `reservaciones`;
 CREATE TABLE `reservaciones` (
-  `idReservacion` int(11) NOT NULL,
+  `idReservacion` int(11) NOT NULL AUTO_INCREMENT,
   `fechaLlegada` date NOT NULL,
   `fechaSalida` date NOT NULL,
   `numAdultos` int(11) NOT NULL,
@@ -198,9 +236,8 @@ CREATE TABLE `reservaciones` (
   `precio` double NOT NULL,
   `idCliente` int(11) NOT NULL,
   PRIMARY KEY (`idReservacion`),
-  KEY `IdClienteReservacion` (`idCliente`),
-  CONSTRAINT `IdClienteReservacion` FOREIGN KEY (`idCliente`) REFERENCES `clientes` (`idCliente`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  KEY `IdClienteReservacion` (`idCliente`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of reservaciones
